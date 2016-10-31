41895cdc306d490ae1916e85446522204feab9b7
michaelv <m.vannieuwenhuize@gmail.com>
2016-10-31 16:45:04 -0700

    Stub files for class

---
+++ src/python/R2LEDCharacterClass.py
@@ -0,0 +1,6 @@
+class R2LEDCharacter(object):
+
+    def __init__(self):
+
+    def __str__(self):
+        return repr(self)
---
+++ src/python/R2LightEngine.py
@@ -0,0 +1,7 @@
+class R2LightEngine(object):
+
+    R2AppObject = None
+
+    def __init__(self,appObject=None):
+
+        R2AppObject = appObject
