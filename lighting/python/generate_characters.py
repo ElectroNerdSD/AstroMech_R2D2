bd53805be35ceb3f7d5fe07a8bc4f5d2af923c26
michaelv <m.vannieuwenhuize@gmail.com>
2016-11-19 22:23:59 -0800

    updated with perl find bin like code, to be able to find json files based on install path

--- lighting/python/generate_characters.py
+++ lighting/python/generate_characters.py
@@ -4,14 +4,16 @@
 comments go here
 """
 
-from R2LightEngine import R2LightEngine
-from R2ErrorClass  import R2Error
 import glob
 import sys
+import os
+from R2LightEngine import R2LightEngine
+from R2ErrorClass  import R2Error
 
 def main():
 
-    print "Initializing character objects..."
+    json_dir = "{}/../json/".format(os.path.abspath(os.path.dirname(__file__)))
+    print "Initializing character objects from {}...".format(json_dir)
 
     try:
 
@@ -26,19 +28,19 @@ def main():
         else:
 
             #parse through the json directories to get all the character files
-            for dir in ["../json/text","../json/numbers","../json/custom"]:
+            for dir_name in ["text", "numbers", "custom"]:
                 #build lists of any json extension files
-                fileList = glob.glob("{}/*.json".format(dir))
+                fileList = glob.glob("{}/{}/*.json".format(json_dir, dir_name))
                 #basic ascii sort
                 fileList.sort()
                 #create object for each json file
-                for file in fileList:
-                    ledObj = lightEngine.loadJson(file)
+                for file_name in fileList:
+                    ledObj = lightEngine.loadJson(file_name)
                     print "{}\n".format(ledObj)
 
     except R2Error as e:
         print e.value
-                
+
 if __name__ == '__main__':
 
-    main();
+    main()
